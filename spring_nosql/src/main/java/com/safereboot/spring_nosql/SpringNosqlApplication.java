9d9b27061299387e09041d3cba335727e24d790e
safereboot <safereboot@gmail.com>
2025-06-01 16:19:16 +0530

    update

---
+++ spring_nosql/src/main/java/com/safereboot/spring_nosql/SpringNosqlApplication.java
@@ -0,0 +1,14 @@
+package com.safereboot.spring_nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNosqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNosqlApplication.class, args);
+        System.out.println("Spring NoSQL Application has started successfully!");
+    }
+
+}
---
+++ spring_nosql/src/test/java/com/safereboot/spring_nosql/SpringNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safereboot.spring_nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNosqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
