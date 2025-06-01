0003855dbc1261186987e6bed4332300b0e5b279
safereboot <safereboot@gmail.com>
2025-06-01 16:18:07 +0530

    first commit

---
+++ src/main/java/com/safereboot/spring_modules/SpringModulesApplication.java
@@ -0,0 +1,14 @@
+package com.safereboot.spring_modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringModulesApplication.class, args);
+        System.out.println("Spring Modules Application has started successfully!");
+    }
+
+}
---
+++ src/test/java/com/safereboot/spring_modules/SpringModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safereboot.spring_modules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
