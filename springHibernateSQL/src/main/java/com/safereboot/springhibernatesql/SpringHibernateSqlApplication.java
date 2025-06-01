6f6f849904df5c28fd3e167cdf5f5577bf62ea80
safereboot <safereboot@gmail.com>
2025-06-01 17:28:05 +0530

    Revert "update"
    This reverts commit 2741ed5e73502cc22a642236ac997982d306654c.

---
+++ springHibernateSQL/src/main/java/com/safereboot/springhibernatesql/SpringHibernateSqlApplication.java
@@ -0,0 +1,13 @@
+package com.safereboot.springhibernatesql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHibernateSqlApplication.class, args);
+    }
+
+}
---
+++ springHibernateSQL/src/test/java/com/safereboot/springhibernatesql/SpringHibernateSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safereboot.springhibernatesql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
