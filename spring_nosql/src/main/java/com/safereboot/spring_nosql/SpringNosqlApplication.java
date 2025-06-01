f1fca7549cfdb3024266d7bc229f9b3d84390c53
safereboot <safereboot@gmail.com>
2025-06-01 16:47:13 +0530

    update

--- spring_nosql/src/main/java/com/safereboot/spring_nosql/SpringNosqlApplication.java
+++ spring_nosql/src/main/java/com/safereboot/spring_nosql/SpringNosqlApplication.java
@@ -9,6 +9,7 @@ public class SpringNosqlApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringNosqlApplication.class, args);
         System.out.println("Spring NoSQL Application has started successfully!");
+
     }
 
 }
